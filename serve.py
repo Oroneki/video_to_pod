8f52a2cbf5358ad375d2948662334f8b13fb5ee8
Paulo Fortes <pauloffn@gmail.com>
2017-11-23 13:31:38 -0300

    Criado servidor simples e separado pra servir o feed e mp3

---
+++ serve.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import os
+import sys
+
+PORT = os.environ.get('SERVER_END').split(':')[-1]
+try:
+    PORT = int(PORT)
+except:
+    print('Verificar a configuração da variável SERVER_END no arquivo .env')
+    sys.exit()
+
+pasta = os.environ.get('PASTA_SERVER')
+
+os.chdir(pasta)
+
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Servindo na porta", PORT)
+httpd.serve_forever()
